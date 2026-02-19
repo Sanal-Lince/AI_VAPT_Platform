ff42cf08d892e9f9d15fd0e70b4e00d362c015e5
Sanal <52928653+Sanal-Lince@users.noreply.github.com>
2026-02-19 01:04:25 -0500

    Add App component with AttackGraph and ReportViewer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import AttackGraph from "./AttackGraph"
+import ReportViewer from "./ReportViewer"
+
+export default function App(){
+ return(
+  <div>
+   <h1>XOXO AI VAPT</h1>
+   <AttackGraph/>
+   <ReportViewer/>
+  </div>
+ )
+}
